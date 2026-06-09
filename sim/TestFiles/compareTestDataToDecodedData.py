2c08a8d5575a370a7758990bf5fbbe03a965f817
DaTofDOT <david@tofft.dk>
2026-06-09 16:51:10 +0200

    comparison between testdata and decoded data

---
+++ sim/TestFiles/compareTestDataToDecodedData.py
@@ -0,0 +1,22 @@
+import pathlib
+
+
+testData = open(pathlib.Path("sim","TestFiles","testData.txt"), "r")
+decoderOut = open(pathlib.Path("sim","TestFiles","decoderOutput.txt"), "r")
+testData.readline() # skip first line
+#line = fileR.readline()
+print("Testing begins")
+lineTest = testData.readline().strip()
+lineDeco = decoderOut.readline.strip()
+i = 1
+while lineTest != "" and lineDeco != "":
+    if lineTest != lineDeco:
+        print("Error on codeword: "+str(i))
+    
+    lineTest = testData.readline().strip()
+    lineDeco = decoderOut.readline().strip()
+    i += 1
+print("Testing done")
+
+testData.close()
+decoderOut.close()
